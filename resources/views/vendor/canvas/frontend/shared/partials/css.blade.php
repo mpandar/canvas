7a53ed5afb62932469430ed094a18e6654275951
StoneMa <mshp_2007@126.com>
2017-12-21 21:51:17 +0800

    canvas

--- resources/views/vendor/canvas/frontend/shared/partials/css.blade.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" type="text/css" href="//cdn.bootcss.com/font-awesome/4.6.3/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="//cdn.bootcss.com/prism/1.5.1/themes/prism.min.css">
-<script type="text/javascript" src="//cdn.bootcss.com/prism/1.5.1/prism.min.js"></script>
-<script type="text/javascript" src="//cdn.bootcss.com/prism/1.5.1/components/prism-php.min.js"></script>
-<link rel="stylesheet" type="text/css" href="{{ elixir('vendor/canvas/assets/css/frontend.css') }}">
